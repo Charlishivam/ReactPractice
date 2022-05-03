2ec69494ab6c10642d82469905073757641b53ac
Charlishivam <gshivam112@gmail.com>
2022-05-03 22:17:42 +0530

    React Project aga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+export default function App() {
+  async function fetchData() {
+    const result = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await result.json();
+    console.log(data);
+  }
+  fetchData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
-
-export default App;
